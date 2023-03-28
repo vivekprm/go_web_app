7ea378b4fd4d537c5f30a1c289c79bea548f7bee
Vivek Mishra <v.mishra@f5.com>
2023-03-28 17:34:05 +0530

    set content type

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type MyHandler struct {
@@ -16,6 +17,21 @@ func (mh *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	data, err := ioutil.ReadFile(string(path))
 
 	if err == nil {
+		var contentType string
+		if strings.HasSuffix(path, ".css"){
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".svg") {
+			contentType = "image/svg+xml"
+		} else {
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content-Type", contentType)
 		w.Write(data)
 	} else {
 		w.WriteHeader(404)
