6cdf01e8ef8e63559f6ab66519bff8283075f898
Vivek Mishra <v.mishra@f5.com>
2023-03-28 22:35:10 +0530

    added subtemplates

---
+++ src/viewmodels/home.go
@@ -0,0 +1,14 @@
+package viewmodels
+
+type Home struct {
+    Title string
+    Active string
+}
+
+func GetHome() Home {
+    result := Home{
+        Title: "Stories",
+        Active: "home",
+    }
+    return result
+}
